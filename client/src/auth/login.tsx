36aa72b1dbc98577df409eb839d1dfe6e266f9e1
innocent <innoxuganda@gmail.com>
2025-02-04 20:11:13 +0300

    auth

---
+++ client/src/auth/login.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./forms/login-form";
+
+const LoginPage = () => {
+    return ( <div className="grid place-items-center w-full h-[100vh]">
+
+    <LoginForm className={"w-[20rem]"}/>
+    </div> );
+}
+ 
+export default LoginPage;
