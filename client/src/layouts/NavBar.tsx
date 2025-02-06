de7a2418d19af412c6dfda746897e459ba95b47a
BwanikaRobert <robertbwanika1@gmail.com>
2025-02-06 12:44:09 +0300

    Add scroll to sidebar

--- client/src/layouts/NavBar.tsx
+++ client/src/layouts/NavBar.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 
 function NavBar() {
   return (
-    <nav className="h-dvh bg-primary   col-[span_1/_span_2] row-span-full">
+    <nav className="h-dvh bg-primary flex flex-col   col-[span_1/_span_2] row-span-full">
       <Logo />
       <Menu />
     </nav>
