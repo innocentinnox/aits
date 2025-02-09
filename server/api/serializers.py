64b9d84a91e9f86713ca7554c74ed59b75f5945d
Wangoda Francis <fwangoda@gmail.com>
2025-02-09 20:46:46 +0300

    Removing redundant code

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import Notes
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
