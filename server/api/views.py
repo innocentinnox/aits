e1882d27a94393e0465b04de006f5bae03a4c563
Wangoda Francis <fwangoda@gmail.com>
2025-02-09 20:40:27 +0300

    created user authentication view

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+
+from .serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
