5fbf1930699fd68f81c5de7f08600c3903ff862b
innocent <innoxuganda@gmail.com>
2025-02-11 20:14:04 +0300

    auth

--- client/src/lib/axios-instance.ts
+++ client/src/lib/axios-instance.ts
@@ -8,11 +8,11 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     
-    // const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
+    const accessToken = JSON.parse(sessionStorage.getItem("accessToken") || "").accessToken;
 
-    // if (accessToken) {
-    //   config.headers.Authorization = `Bearer ${accessToken}`;
-    // }
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
     return config;
   },
   (err) => Promise.reject(err)
--- client/src/page/dashboard/DashBoard.tsx
+++ client/src/page/dashboard/DashBoard.tsx
@@ -2,8 +2,6 @@ import { Send } from "lucide-react";
 import Card from "./CardItem";
 import CardGrid from "./CardGrid";
 import Table from "./Table/Table";
-import Filter from "./Table/Filter/Filter";
-import FilterGrid from "./Table/Filter/FilterGrid";
 
 export default function DashBoard() {
   return (
