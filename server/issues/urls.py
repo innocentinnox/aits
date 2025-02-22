06ff843dd7e9839da54e22adf535ca3588bf3743
Wangoda Francis <fwangoda@gmail.com>
2025-02-22 22:38:25 +0300

    created url for creating issues categories

--- server/issues/urls.py
+++ server/issues/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IssueCreateView, IssueDetailView, IssueUpdateView
+from .views import IssueCreateView, IssueDetailView, IssueUpdateView, IssueCategoryView
 
 urlpatterns = [
     path('issues/', IssueCreateView.as_view(), name='issue-create'),
+    path('issue-categories/', IssueCategoryView.as_view(), name='issue-create-category'),
     path('issues/<str:token>/', IssueDetailView.as_view(), name='issue-detail'),
     path('issues/<str:token>/update/', IssueUpdateView.as_view(), name='issue-update'),
 ]
