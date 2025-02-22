cb222fc5880db12bb7699d687b96123e11a97620
Wangoda Francis <fwangoda@gmail.com>
2025-02-22 22:17:39 +0300

    Issue create view for students

--- server/issues/views.py
+++ server/issues/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth import get_user_model
+from .models import Issue
+from .serializers import IssueSerializer
 
-# Create your views here.
+User = get_user_model()
+
+# Issue view: --> Only foe students
+class IssueCreateView(generics.CreateAPIView):
+    serializer_class = IssueSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.role != 'Student':
+            raise PermissionDenied("Only students can create issues.")
+        
+        # Find Registrar: --> of students college
+        registrar = User.objects.filter(role='registrar', college=user.college).first()
+        serializer.save(created_by=user, assigned_to=registrar)
