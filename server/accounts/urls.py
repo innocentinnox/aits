5641afc2cf38a8bd16562cc5f984933586175c45
Wangoda Francis <fwangoda@gmail.com>
2025-02-24 09:26:03 +0300

    Created the notification url endpoint

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, logout_view, CollegeView, ProfileUpdateView
+from .views import RegisterView, LoginView, logout_view, CollegeView, ProfileUpdateView, NotificationListView
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('colleges/', CollegeView.as_view(), name='colleges'),
     path('profile/', ProfileUpdateView.as_view(), name='profile-update'),
+    path('notifications/', NotificationListView.as_view(), name='notifications'),
 ]
 
