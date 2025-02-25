f47084296d4e22d7f0f695cac4d79a44df74d882
Wangoda Francis <fwangoda@gmail.com>
2025-02-25 17:59:20 +0300

    Added token refresh url

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, logout_view, CollegeView, ProfileUpdateView, NotificationListView, status_view
+from .views import RegisterView, LoginView, logout_view, CollegeView, ProfileUpdateView, NotificationListView, status_view, TokenRefreshCookieView
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('colleges/', CollegeView.as_view(), name='colleges'),
     path('profile/', ProfileUpdateView.as_view(), name='profile-update'),
     path('notifications/', NotificationListView.as_view(), name='notifications'),
+    path('token/refresh/', TokenRefreshCookieView.as_view(), name='refresh-token'),
     path('status/', status_view, name='status'),
 ]
 
