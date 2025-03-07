39e02367377e07b1ed07035b8472d285833c4ae9
BwanikaRobert <robertbwanika1@gmail.com>
2025-03-07 18:11:16 +0300

    Add project logo

--- client/src/layouts/Logo.tsx
+++ client/src/layouts/Logo.tsx
@@ -1,12 +1,13 @@
 import { Database } from "lucide-react";
+import LogoIcon from "./LogoIcon";
 
 function Logo() {
   const logoIcon = " w-[2.5rem] h-[2.5rem] sm:w-[3.5rem] sm:h-[3.5rem]";
   return (
     <div className="flex items-center gap-1 sm:p-4 sm:pl-0  ">
-      <Database className="  w-[2.5rem] h-[2.5rem] sm:w-[3.5rem] sm:h-[3.5rem] stroke-current text-primary " />
+      <LogoIcon />
 
-      <span className=" text-[1.6rem]   md:text-[1.8rem] text-primary">
+      <span className=" text-[1.2rem]   md:text-[1.2rem] text-primary">
         AITS
       </span>
     </div>
