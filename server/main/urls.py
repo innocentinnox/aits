94f22df4a9054e951e557b9727368afb3ac0444b
innocent <innoxuganda@gmail.com>
2025-03-11 14:55:25 +0300

    debug

--- server/main/urls.py
+++ server/main/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-
-import settings 
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/', include('issues.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
