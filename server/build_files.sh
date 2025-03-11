6b514e5008295e5f6dc7c0599a05544d5c5f18b8
innocent <innoxuganda@gmail.com>
2025-03-11 12:53:25 +0300

    debu

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt
+#!/bin/bash
+
+echo "Installing requirements.txt"
+python3 -m pip install -r requirements.txt
+
+echo "Collecting static files"
 python3 manage.py collectstatic --noinput
