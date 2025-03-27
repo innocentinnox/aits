05e407b114f01529aa5063d5c4acee4236253f09
Wangoda Francis <fwangoda@gmail.com>
2025-03-27 15:30:33 +0300

    Re added dummy send email function

--- server/accounts/utils.py
+++ server/accounts/utils.py
@@ -53,6 +53,18 @@ mailer = Mail(
         password=settings.EMAIL_HOST_PASSWORD
 )
 
+def send_notification(recipient, subject, message):
+     # Create: Notification record in database
+     Notification.objects.create(recipient=recipient, message=message)
+     print('Email sent: ', message)
+     # send_email(
+     #     subject,
+     #     message,
+     #     'no-reply@aits.messages.com',
+     #     [recipient.email],
+     #     fail_silently=False,
+     # )
+
 
 
 
