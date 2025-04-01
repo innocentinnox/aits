bb053ecd99338cf2133933c440ab1fdd01e10ca6
innocent <innoxuganda@gmail.com>
2025-04-01 17:10:45 +0300

    courses mainly under school

--- server/entrypoint.prod.sh
+++ server/entrypoint.prod.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 
 python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 main.wsgi:application
+exec python -m gunicorn --bind 0.0.0.0:8000 --workers 3 main.wsgi:application
