01027034be83130bb19ad41421933d98c392e8e4
Robert <robertbwanika1@gmail.com>
2025-04-03 06:52:47 +0300

    finish user logout

--- client/src/layouts/LogoutButton.tsx
+++ client/src/layouts/LogoutButton.tsx
@@ -1,1 +1,37 @@
+import { LogOut } from "lucide-react";
 
+function LogoutButton({
+  handler,
+  name,
+}: {
+  handler: Function;
+  name: string | undefined;
+}) {
+  return (
+    <li
+      className={
+        `${
+          !name
+            ? ""
+            : "py-3 pl-6 pr-4 text-zinc-900 hover:bg-zinc-200 hover:text-zinc-700"
+        }` + " flex items-center gap-2  cursor-pointer "
+      }
+      onClick={() => {
+        // call logout function
+        handler();
+      }}
+    >
+      <span>
+        <LogOut size={!name ? 16 : 24} />
+      </span>
+      <span
+        className={` ${
+          !name ? "text-[14px]" : "text-[1rem]"
+        } capitalize text-semibold `}
+      >
+        {name || "Logout"}
+      </span>
+    </li>
+  );
+}
+export default LogoutButton;
