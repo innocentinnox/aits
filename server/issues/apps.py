cde5b49271e38478c8af77a55fd8529b3c9c49f2
Wangoda Francis <fwangoda@gmail.com>
2025-04-14 19:34:47 +0300

    registered signals in accounts app

--- server/issues/apps.py
+++ server/issues/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class IssuesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'issues'
+    
+    def ready(self):
+        # Import and register signals
+        import accounts.signals
