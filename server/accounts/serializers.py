65a451ad83c453b248b9683eb07f34e64e6a703f
Wangoda Francis <fwangoda@gmail.com>
2025-04-14 08:57:52 +0300

    removed redundant code

--- server/accounts/serializers.py
+++ server/accounts/serializers.py
@@ -160,8 +160,6 @@ class VerifyTokenSerializer(serializers.Serializer):
         if token_obj.expires_at < timezone.now():
             raise serializers.ValidationError("Token has expired.")
 
-        if token_obj.is_used:
-            raise serializers.ValidationError("Token has already been used.")
 
         if token_obj.code != data['code']:
             raise serializers.ValidationError("Invalid code.")
