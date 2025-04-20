c659b9e600c3e6110c114f74fbd79c6494a98fb8
IANSEZI <miansezi87@gmail.com>
2025-04-20 01:36:35 -0700

    some comments added

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ jsonfile.writeFile(FILE_PATH, data, ()=>{
 // git commit --date=""
 simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }).push();
 });
+
+
+//ssdsdsdsdsdsdsdsd
+// git commit --date=""
+// git push origin master
+// git push origin master --force
+// git push origin master --force --no-verify
+// git push origin master --force --no-verify --quiet
+// git push origin master --force --no-verify --quiet --progress
+// git push origin master --force --no-verify --quiet --progress --verbose
+// git push origin master --force --no-verify --quiet --progress --verbose --dry-run
+// git push origin master --force --no-verify --quiet --progress --verbose --dry-run --tags
+// git push origin master --force --no-verify --quiet --progress --verbose --dry-run --tags --all
+// git push origin master --force --no-verify --quiet --progress --verbose --dry-run --tags --all --prune
