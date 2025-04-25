6ded67fc58da9f2dd934509000c4691e35845cd2
IANSEZI <miansezi87@gmail.com>
2025-04-25 00:59:01 -0700

    update data.json structure and write logic in index.js to include timestamp

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const jsonfile = require('jsonfile')
+const moment = require('moment');
 
 const FILE_PATH = './data.json';
 
-const DATE = 
+const DATE = moment().format();
+
+const data = {
+    date: DATE 
+}
+
+jsonfile.writeFile(FILE_PATH, data);
