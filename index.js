25bfab2e87fec50dd90bb5238805aced2a0645c6
IANSEZI <miansezi87@gmail.com>
2025-04-25 01:15:32 -0700

    fix: update date calculation to use offset for data.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const DATE = moment().subtract(30,'d').format();
 
 const data = {
     date: DATE 
