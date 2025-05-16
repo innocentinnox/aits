1fd1887b2bb487b3efe36e43a52b0eed916c270e
[Ian Matsiko] <[miansezi87@gmail.com]>
2025-05-16 02:39:11 -0700

    Test class definition

--- server/accounts/tests.py
+++ server/accounts/tests.py
@@ -10,3 +10,15 @@ import jwt
 from django.conf import settings
 from django.urls import reverse
 
+class AITSTestCase(APILiveServerTestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpass', email='test@example.com')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        self.college = College.objects.create(name='Engineering')
+        self.school = School.objects.create(name='Computer Science', college=self.college)
+        self.department = Department.objects.create(name='Software Engineering', school=self.school)
+        self.course = Course.objects.create(name='BSc Software Engineering', department=self.department)
+        self.course_unit = CourseUnit.objects.create(name='Web Development', course=self.course)
+        self.notification = Notification.objects.create(user=self.user, message='Test notification')
