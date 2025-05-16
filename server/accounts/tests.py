8e87bced6a28db81e24973648c85188e836543d9
[Ian Matsiko] <[miansezi87@gmail.com]>
2025-05-16 02:38:21 -0700

    Initializing tests

--- server/accounts/tests.py
+++ server/accounts/tests.py
@@ -1,21 +1,12 @@
 from django.test import TestCase
-
-# Create your tests here.
-# The test case should cover the following scenarios:
-# 1. Test that the CollegeSerializer correctly serializes and deserializes a College instance.
-# 2. Test that the CollegeListAPIView returns a list of colleges.
-# 3. Test that the SchoolListAPIView returns a list of schools.
-# 4. Test that the DepartmentListAPIView returns a list of departments.
-# 5. Test that the CourseListAPIView returns a list of courses.
-# 6. Test that the CourseUnitesListAPIView returns a list of course units.
-# 7. Test that the SendEmailAPIView sends an email.
-# 8. Test that the SignupAPIView creates a new user.
-# 9. Test that the VerifyTokenAPIView verifies a token.
-# 10. Test that the PasswordResetRequestAPIView sends a password reset email.
-# 11. Test that the LogoutView logs out a user.
-# 12. Test that the LoginView logs in a user.
-# 13. Test that the ProfileUpdateView updates a user's profile.
-# 14. Test that the NotificationListView returns a list of notifications.
-# 15. Test that the TokenRefreshCookieView refreshes a token.
-# 16. Test that the status_view returns a status.
+from rest_framework.test import APIClient, APILiveServerTestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.core import mail
+from issue_tracker.models import College, School, Department, Course, CourseUnit, Notification
+from issue_tracker.serializers import CollegeSerializer
+from rest_framework.authtoken.models import Token
+import jwt
+from django.conf import settings
+from django.urls import reverse
 
